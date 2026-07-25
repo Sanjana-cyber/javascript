d8b800bf55d1698e6bb190b5a66db889d50cc7bb
Mohan pandey <sanjanapandey29256@gmail.com>
2026-07-25 17:16:56 +0530

    cxg

--- javascript/simon_game/simon.js
+++ javascript/simon_game/simon.js
@@ -1,11 +1,35 @@
 let level=0;
 let start=false;
 let heading=document.querySelector("H2")
+let array=["red","green","yellow","purple"]
+
 document.addEventListener("keypress",function()
 {
     if (start==false)
     {
     heading.innerHTML="Game start! you are at level 0"
-  start=true;}
+  start=true;
+level_up()}
+
 
 })
+function button_flash(btn){
+  btn.classList.add("flash");
+  setTimeout(
+    function(){
+      btn.classList.remove("flash")
+    },500
+  )
+ 
+}
+ function level_up()
+ {
+  level++;
+  heading.innerHTML=`${level}`;
+  let random_idx=Math.floor(Math.random()*3)
+ let random_class=array[random_idx]
+ let random_btn=document.querySelector(`.${random_class}`)
+
+  button_flash(random_btn);
+ }
+ 
